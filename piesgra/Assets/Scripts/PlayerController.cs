c538b8dc7985d3cd25e96e8828a9163b9972e69c
Maksymilian Tym <tymszczur@gmail.com>
2020-12-22 23:01:35 +0100

    Made all the dog breeds function properly (just the mechanics, no spirtes yet).

--- piesgra/Assets/Scripts/PlayerController.cs
+++ piesgra/Assets/Scripts/PlayerController.cs
@@ -12,17 +12,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horizontalAxis;
     public bool isGrounded;
     [SerializeField] private Transform groundDetector;
+    public bool breathingToExist;
+    [SerializeField]private int currentDogIndex;
+
+    [SerializeField]private float timeLeftToBreathe;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        alphaWolf();
+        timeLeftToBreathe = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace)) Death(); //DEBUG!!!! USUNAC POTEM!!!!
+        DebugStuff(); //DO USUNIECIA
+        if(breathingToExist) BreatheTest(10);
         isGrounded = Physics2D.OverlapCircle(groundDetector.position, 0.15f, groundLayer);
         horizontalAxis = Input.GetAxis("Horizontal");
 
@@ -50,31 +56,55 @@ public class PlayerController : MonoBehaviour
     void alphaWolf()
     {
         Debug.Log("PIES TO TERAZ WILK ALFA");
+        speed = 12;
+        jumpForce = 500;
+        breathingToExist = false;
+        currentDogIndex = 0;
     }
 
     void wolf()
     {
         Debug.Log("PIES TO TERAZ WILK");
+        speed = 10;
+        jumpForce = 450;
+        breathingToExist = false;
+        currentDogIndex = 1;
     }
 
     void mastif()
     {
         Debug.Log("PIES TO TERAZ MASTIF");
+        speed = 8;
+        jumpForce = 400;
+        breathingToExist = false;
+        currentDogIndex = 2;
     }
 
     void kundel()
     {
         Debug.Log("PIES TO TERAZ KUNDEL");
+        speed = 6;
+        jumpForce = 300;
+        breathingToExist = false;
+        currentDogIndex = 3;
     }
 
     void owczarek()
     {
         Debug.Log("PIES TO TERAZ OWCZAREK");
+        speed = 4;
+        jumpForce = 250;
+        breathingToExist = false;
+        currentDogIndex = 4;
     }
 
     void mops()
     {
         Debug.Log("PIES TO TERAZ MOPS");
+        speed = 2;
+        jumpForce = 100;
+        breathingToExist = true;
+        currentDogIndex = 5;
     }
 
     public void Death()
@@ -83,5 +113,38 @@ public class PlayerController : MonoBehaviour
         //MAYBE PARTICLES HERE?
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("You died.");
+    }
+
+    void BreatheTest(float timeToBreathe)
+    {
+        timeLeftToBreathe -= Time.deltaTime;
+        if(Input.GetButtonDown("Breathe"))
+        {
+            timeLeftToBreathe = timeToBreathe;
+        }
+        if(timeLeftToBreathe < 5)
+        {
+            Debug.Log("5 seconds left!");
+        }
+        if(timeLeftToBreathe < 0)
+        {
+            Death();
+        }
+    }
+
+
+
+
+    void DebugStuff()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace)) Death();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            changeDogLevel(currentDogIndex + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            changeDogLevel(currentDogIndex - 1);
+        }
     }
 }
