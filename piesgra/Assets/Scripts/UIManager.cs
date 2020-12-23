ae275c032e03bf86fba9f6a8632bbc1a2e295bf6
Maksymilian Tym <tymszczur@gmail.com>
2020-12-23 01:48:06 +0100

    Added a settings menu

--- piesgra/Assets/Scripts/UIManager.cs
+++ piesgra/Assets/Scripts/UIManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class UIManager : MonoBehaviour
 {
 
     public GameObject settingsPanel;
+    public AudioMixer mixer;
 
     void Start()
     {
@@ -33,5 +35,15 @@ public class UIManager : MonoBehaviour
     public void QuitSettings()
     {
         settingsPanel.SetActive(false);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("GenVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
